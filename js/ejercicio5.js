9790dc85223ba89848f00be62c2cdbca670ad11b
cintia rivera <riveracintia67@gmail.com>
2023-07-25 16:17:34 -0300

    no sabemos nada

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -3,3 +3,24 @@
 ✓ Si trabaja 180 horas o menos, se le paga $ 560,00 la hora.
 ✓ Si trabaja más de 180 se le paga un plus de $ 200,00 por cada hora adicional.
 */
+
+function calcularSalarioMensual(horasTrabajadas, salarioPorHora) {
+    const horasPorMes = 160; // Suponiendo que hay 160 horas de trabajo en un mes estándar (puedes ajustar este valor según tus necesidades)
+    let salarioMensual = horasTrabajadas * salarioPorHora;
+  
+    if (horasTrabajadas > horasPorMes) {
+      // Si el empleado trabajó horas extras, calculamos el salario con el pago correspondiente a las horas extras.
+      const horasExtras = horasTrabajadas - horasPorMes;
+      const salarioHoraExtra = salarioPorHora * 1.5; // Suponiendo un 50% de aumento para horas extras
+      const salarioPorHorasExtras = horasExtras * salarioHoraExtra;
+      salarioMensual += salarioPorHorasExtras;
+    }
+  
+    return salarioMensual;
+  }
+  
+  // Ejemplo de uso:
+  const horasTrabajadas = 180;
+  const salarioPorHora = 10;
+  const salarioMensual = calcularSalarioMensual(horasTrabajadas, salarioPorHora);
+  console.log(`El salario mensual del empleado es: $${salarioMensual}`);
