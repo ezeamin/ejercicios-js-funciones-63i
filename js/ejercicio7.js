1b801f5ab897dce8b528b45495af06e7e13f57ac
Agustincito <nicolasacaceres92@gmail.com>
2023-07-25 16:19:52 -0300

    Esperemos que todo funcione

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -6,3 +6,25 @@ Esqui -> 32 < Temperatura <= 35|
 Otros -> Temperatura <= 32
 ✓ Nota: Grados Fahrenheit = 9/5 * (grados Celsius) + 32
 */
+
+
+const deporte = (gradosCelsius) =>{
+let gradosFarenheit = 0;
+    gradosFarenheit = 9 % 5 * gradosCelsius + 32
+
+if(gradosFarenheit > 85){
+    console.log("natacion")
+} 
+else if(gradosFarenheit <=85 && gradosFarenheit > 35){
+    console.log("Tenis")
+} 
+else if(gradosFarenheit > 32 && gradosFarenheit <=35){
+    console.log("Esquí")
+}
+else if (gradosFarenheit <= 32){
+    console.log("Otros")
+}
+}
+gradosCelsius=prompt("ingrese una temperatura")
+const resultado = deporte(gradosCelsius)
+console.log(resultado)
