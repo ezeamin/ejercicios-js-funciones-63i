96071ccde8dddb2c9d771cefdf1c4b0e44039d8e
Martin Cardozo <tiin.-@hotmail.es>
2023-07-25 23:01:02 -0300

    Ejercicios de numeros primos

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,3 +1,18 @@
 /*
 1- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
 */
+
+
+
+let num = parseInt(prompt("Ingrese un numero"));
+
+const parImpar = (num) =>{
+    if(num % 2 === 0){
+        console.log("el numero es par")
+      }else{
+        console.log("el numero es impar")
+      }
+}
+
+const res = parImpar(num);
+console.log(res);
--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,3 +1,19 @@
 /*
 2- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
 */
+
+
+let texto = prompt("Ingrese una texto");
+const cadenaDeTexto = (texto) =>{
+  if(texto === texto.toUpperCase()){
+    console.log("el texto contiene mayusculas")
+  }else if(texto === texto.toLowerCase ()){
+    console.log("el texto contiene minusculas")
+  }else{
+    console.log("el texto contiene minusculas y mayusculas")
+  }
+}
+
+const res = cadenaDeTexto(texto);
+
+console.log(res);
